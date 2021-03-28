7a4d3633ef3d1074f567a605252cdca619c185fb
Conrado Pinheiro <conradopsa@gmail.com>
2021-03-28 09:49:37 -0300

    Gradient Support & Poppins Font & Center Colors

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,38 +1,53 @@
-import React, {useState, useContext} from "react";
-import { View, Text, Image } from "react-native";
-import { TextInput, Button } from "react-native-paper";
-import { theme } from "../../theme";
-import style from "./Login.style";
+import React, {useState, useContext} from 'react';
+import {View, Text, Image} from 'react-native';
+import {TextInput, Button} from 'react-native-paper';
+import {theme, colors} from '../../theme';
+import style from './Login.style';
 import AuthContext from '../../contexts/auth';
+import {LinearTextGradient} from 'react-native-text-gradient';
 
-import LogoSVG from "../../assets/images/refocus-center-logo.svg";
-import GoogleSVG from "../../assets/images/icons/google-icon.svg"
+import LogoSVG from '../../assets/images/refocus-center-logo.svg';
+import GoogleSVG from '../../assets/images/icons/google-icon.svg';
 
-import {widthPercentageToDP as wd, heightPercentageToDP as hg} from "react-native-responsive-screen"
+import {
+  widthPercentageToDP as wd,
+  heightPercentageToDP as hg,
+} from 'react-native-responsive-screen';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {ScrollView} from 'react-native-gesture-handler';
 
 export default function Login() {
-  const { handleLogin } = useContext(AuthContext);  
-  const [ credentials, setCredentials ] = useState({
-    email: "",
-    password: ""
+  const {handleLogin} = useContext(AuthContext);
+  const [credentials, setCredentials] = useState({
+    email: '',
+    password: '',
   });
   const login = () => {
     handleLogin(credentials);
-  }
+  };
+
+  const {MainDegrade60: md60} = colors;
 
   return (
-      <View style={style.mainView}>
+    <SafeAreaView>
+      <ScrollView style={style.mainView}>
+        <LinearTextGradient
+          locations={md60.locations}
+          colors={md60.colors}
+          start={md60.start}
+          end={md60.end}>
+          <Text style={{fontFamily: 'Poppins-Regular', fontSize: 80}}>Refocus Center</Text>
+        </LinearTextGradient>
+
         <View style={[style.centerView, style.cestouView]}>
           <LogoSVG width={hg(45)} height={hg(32.38)}></LogoSVG>
         </View>
-
         <TextInput
           placeholderTextColor={theme.colors.text}
           style={style.textInput}
           mode="outlined"
           placeholder="Seu email"
         />
-
         <TextInput
           placeholderTextColor={theme.colors.text}
           style={style.textInput}
@@ -40,16 +55,13 @@ export default function Login() {
           secureTextEntry={true}
           placeholder="Sua senha"
         />
-
         <Button
           style={style.button}
           labelStyle={style.buttonLabel}
           mode="contained"
-          onPress={login}
-        >
+          onPress={login}>
           Entrar
         </Button>
-
         <View style={[style.socialMediaView, style.centerView]}>
           <Text style={style.text}>Ou entre com</Text>
 
@@ -58,9 +70,11 @@ export default function Login() {
           </View>
 
           <Text style={style.text}>
-            Ainda não possui conta? <Text style={style.textBold}>Crie agora</Text>
+            Ainda não possui conta?{' '}
+            <Text style={style.textBold}>Crie agora</Text>
           </Text>
         </View>
-      </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- src/theme.tsx
+++ src/theme.tsx
@@ -1,4 +1,5 @@
-import { DefaultTheme } from "react-native-paper";
+import {DefaultTheme} from 'react-native-paper';
+import LinearGradient from 'react-native-linear-gradient';
 
 type PaperTheme = ReactNativePaper.Theme;
 
@@ -6,10 +7,10 @@ export const theme: PaperTheme = {
   ...DefaultTheme,
   colors: {
     ...DefaultTheme.colors,
-    primary: "#1CD69D", 
-    background: "#fff",
-    text: "#757575",
-    placeholder: "#dddddd"
+    primary: '#1CD69D',
+    background: '#fff',
+    text: '#757575',
+    placeholder: '#dddddd',
     /*accent: "red",
     backdrop: "red",
     surface: "red",
@@ -19,5 +20,49 @@ export const theme: PaperTheme = {
     onBackground: "red",
     disabled: "red",
     error: "red"*/
-  }
+  },
+};
+
+interface Point {
+  x: number;
+  y: number;
+}
+
+interface Gradient {
+  locations: number[];
+  colors: string[];
+  start: Point;
+  end: Point;
+}
+
+export const colors = {
+  // mainDegrade100: "linear-gradient(88.78deg, #DA5AFA -34.1%, #3570EC 113.16%)",
+  // mainDegrade60: "linear-gradient(88.78deg, rgba(218, 90, 250, 0.6) -34.1%, rgba(53, 112, 236, 0.6) 113.16%)",
+  MainDegrade100: {
+    locations: [0, 1],
+    colors: ['#DA5AFA', '#3570EC'],
+    start: {x: 0, y: 0},
+    end: {x: 1, y: 1},
+  } as Gradient,
+  MainDegrade60: {
+    locations: [0, 1],
+    colors: ['rgba(218, 90, 250, 0.6)', 'rgba(53, 112, 236, 0.6)'],
+    start: {x: 0, y: 0},
+    end: {x: 1, y: 1},
+  } as Gradient,
+  
+  lightBlue92: '92B2F5',
+  darkPurple: '555187',
+
+  black0B: '0B0E26',
+  black2F: '2F2F2F',
+  black41: '414048',
+  black50: '505664',
+  gray80: '807E90',
+  grayC2: 'C2C0D3',
+  whiteF2: 'F2F0FF',
+  white: 'FFFFFF',
+
+  green1C: '1CD69D',
+  redFF: 'FF9BB3',
 };
