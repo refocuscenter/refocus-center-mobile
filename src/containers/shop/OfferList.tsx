a97002b8aeb4311178250ca38519cd4672590acf
Conrado Pinheiro <conradopsa@gmail.com>
2021-04-08 23:04:23 -0300

    OfferList

--- src/containers/shop/OfferList.tsx
+++ src/containers/shop/OfferList.tsx
@@ -1,6 +1,17 @@
-import React from 'react';
-import {ScrollView, StyleProp, StyleSheet, View, ViewStyle} from 'react-native';
+import React, { useState } from 'react';
+import {
+  ScrollView,
+  StyleProp,
+  StyleSheet,
+  View,
+  ViewStyle,
+  FlatList,
+} from 'react-native';
+import {BlankItem} from '../../components/shop/BlankItem';
 import {OfferItem} from '../../components/shop/OfferItem';
+import {TextGradient} from '../../components/TextGradient';
+import {ViewGradient} from '../../components/ViewGradient';
+import {colors} from '../../theme';
 import {XOR} from '../../types/app/operators';
 import {Combo, Offer} from '../../types/domain/interfaces';
 
@@ -12,28 +23,55 @@ const style = StyleSheet.create({
     flexDirection: 'row',
   },
   main: {
-    marginHorizontal: 10
+    marginHorizontal: 10,
+  },
+  item: {
+    margin: 4,
   },
 });
 
 interface OfferListProps {
   offers: XOR<Offer, Combo<Offer>>[];
-  style: StyleProp<ViewStyle>
+  limit?: number;
+  style?: StyleProp<ViewStyle>;
 }
 
 export function OfferList(props: OfferListProps) {
-  const {offers} = props;
+  const {offers, limit} = props;
 
   return (
-    <ScrollView
-      style={props.style}
-      showsVerticalScrollIndicator={false}
-      showsHorizontalScrollIndicator={false}>
-      <View style={[style.main, style.inlineView]}>
-        {offers.map((o, i) => (
-          <OfferItem key={'offer-' + i} offer={o} />
-        ))}
-      </View>
-    </ScrollView>
+    <View style={props.style}>
+      <FlatList
+        numColumns={2}
+        data={createItems(props)}
+        keyExtractor={(_, i) => 'offer-item-' + i}
+        style={style.main}
+        renderItem={renderItem}
+      />
+    </View>
   );
+}
+
+function createItems(props: OfferListProps): XOR<Offer, Combo<Offer>>[] {
+  const {offers, limit} = props;
+
+  return limitItems(offers, limit).map((o, i, arr) =>
+    limit && i == limit - 1 ? 'see-more' : o,
+  );
+}
+
+function renderItem({item}: {item: XOR<Offer, Combo<Offer>> | 'see-more'}) {
+  return item !== 'see-more' ? (
+    <OfferItem
+      containerStyle={style.item}
+      offer={item as XOR<Offer, Combo<Offer>>}
+    />
+  ) : (
+    <BlankItem containerStyle={style.item}>Ver mais</BlankItem>
+  );
+}
+
+function limitItems(items: any[], limit?: number) {
+  if (limit !== undefined) return items.slice(0, limit);
+  else return items;
 }
