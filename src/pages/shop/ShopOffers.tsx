9ce4b4262e77a1c801d052761b5045cd8531aaff
Conrado Pinheiro <conradopsa@gmail.com>
2021-04-09 06:40:35 -0300

    page shop offers

--- src/pages/shop/ShopOffers.tsx
+++ src/pages/shop/ShopOffers.tsx
@@ -1,220 +1,76 @@
-import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native";
-import { ScrollView } from "react-native-gesture-handler";
-import { Avatar, Button, Chip, TextInput } from "react-native-paper";
-import { paperTheme } from "../../theme";
-import { Product, User } from "../../types/domain/interfaces";
-import IconMaterialCommunityIcons from "react-native-vector-icons/MaterialCommunityIcons";
-import { produtosMercadinho } from "../../mocks/productsCategories";
-import {widthPercentageToDP as wd, heightPercentageToDP as hg} from "react-native-responsive-screen"
+import React from 'react';
+import {View, Text, Image, StyleSheet} from 'react-native';
+import {ScrollView} from 'react-native-gesture-handler';
+import {Avatar, Button, Chip, TextInput} from 'react-native-paper';
+import {colors, fonts, paperTheme} from '../../theme';
+import {
+  Combo,
+  Offer,
+  OfferXorCombo,
+  Product,
+  User,
+} from '../../types/domain/interfaces';
+import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import {produtosMercadinho} from '../../mocks/productsCategories';
+import {
+  widthPercentageToDP as wd,
+  heightPercentageToDP as hg,
+} from 'react-native-responsive-screen';
+import {XOR} from '../../types/app/operators';
+import {SafeAreaView} from 'react-native-safe-area-context';
+import {StackScreenProps} from '@react-navigation/stack';
+import {ShopStackParamList} from '../../routes/ShopRoutes';
+import {ShopTopMenu} from '../../containers/shop/ShopTopMenu';
+import {OfferList} from '../../containers/shop/OfferList';
+import ShopProfile from '../../containers/shop/ShopProfile';
 
 const style = StyleSheet.create({
-  inlineView: {
-      flexWrap: "wrap",
-      alignItems: "flex-start",
-      flexDirection: "row",
+  main: {
+    flex: 1,
+    backgroundColor: colors.white,
   },
-  mainView: {
-      marginTop: 40,
-      marginHorizontal: 15
-  },
-  topView: {
-      justifyContent: "space-between"
-  },
-  title: {
-      fontSize: 18,
-      alignSelf: "center"
-  },
-  roundButton: {
-      borderRadius: 50      
-  },
-  textButton: {
-      color: "#fff"
-  },
-  searchInput: {
-      marginTop: 10
-  },
-  tagsContainer: {
-      marginVertical: 10,
-      marginBottom: 15
-  },
-  listProductView: {
-      marginBottom: 190
-  }
-})
-
-interface ProductListProps {
-  [key: string]: any;
-}
-
-interface ProductItemProps {
-  product: Product;
-  [key: string]: any;
-}
-
-const styleProductItem = StyleSheet.create({
-  mainView: {
-    width: wd(42.01),
-    backgroundColor: "#fff",
-    margin: wd(2),
-    padding: wd(4),
-    borderRadius: 10
-  },
-  productView: {
-    alignItems: "center"
-  },
-  textProductView: {
-    overflow: "hidden",
-    height: wd(10),
-    justifyContent: "center"
-  },
-  textProduct: {
-    textAlign: "center"
-  },
-  inlineView: {
-    flexWrap: "wrap",
-    alignItems: "flex-start",
-    flexDirection: "row",
-  },
-  bottomView: {
-    justifyContent: "space-between",
-    marginTop: 10
-  },
-  valueText: {
-    color: paperTheme.colors.primary,
-    alignSelf: "center"
-  }
-})
-
-function ProductItem({ product }: ProductItemProps) {
-  return (
-    <View style={styleProductItem.mainView}>
-      <View style={styleProductItem.productView}>
-        <Avatar.Image size={wd(25)} source={{ uri: product.image }} />
-      <View style={styleProductItem.textProductView}>
-        <Text style={styleProductItem.textProduct}>{product.name}</Text>
-      </View>
-      </View>      
-      <View style={[styleProductItem.bottomView, styleProductItem.inlineView]}>
-        <Text style={styleProductItem.valueText}>R$ {product.value}</Text>
-        <IconMaterialCommunityIcons size={25} color={paperTheme.colors.primary} name="basket-fill" />
-      </View>
-    </View>
-  );
-}
-
-const styleTag = StyleSheet.create({
-  tag: {
-    marginHorizontal: 5,
-    borderRadius: 50,
-    alignItems: "center",
-    borderColor: "#d6d6d6",
-  },
-  tagText: {
-    paddingHorizontal: 10,
-    textTransform: "none",
-  },
-  tagSelected: {
-    marginHorizontal: 5,
-    borderRadius: 50,
-    alignItems: "center",
-    borderColor: "#d6d6d6",
-  },
-  tagTextSelected: {
-    color: "#fff",
-    paddingHorizontal: 10,
-    textTransform: "none",
+  serviceText: {
+    marginTop: 30,
+    marginLeft: 20,
+    fontFamily: fonts.semiBoldFontFamily,
+    fontSize: 15,
+    color: colors.darkPurple,
   },
 });
 
-interface TagProps {
-  selected?: boolean;
-  onPress?: () => void;
-  children: any;
-  [key: string]: any;
-}
+type ShopOffersProps = StackScreenProps<ShopStackParamList, 'ShopHome'>;
 
-export function Tag(props: TagProps) {
-  function renderTag() {
-    if (props.selected)
-      return (
-        <Button
-          mode="contained"
-          style={styleTag.tagSelected}
-          labelStyle={styleTag.tagTextSelected}
-          onPress={props.onPress}
-        >
-          {props.children}
-        </Button>
-      );
-    else
-      return (
-        <Button
-          mode="outlined"
-          style={styleTag.tag}
-          labelStyle={styleTag.tagText}
-          onPress={props.onPress}
-        >
-          {props.children}
-        </Button>
-      );
-  }
-
-  return <View>{renderTag()}</View>;
-}
-
-export default function ProductList(props: ProductListProps) {
-  
-  //Transformar isso aqui em use context ou reducer
-  //const user = user;
-  const productCategoryList = produtosMercadinho;
+export default function ProductList(props: ShopOffersProps) {
+  const {route, navigation} = props;
+  const {unitStore, userAccountStore, services} = route.params;
 
   return (
-    <View style={style.mainView}>
-
-
-      <View style={[style.topView, style.inlineView]}>
-        <Text style={style.title}>Meus Produtos</Text>
-        <Button
-          icon="arrow-left"
-          style={style.roundButton}
-          mode="contained"
-          labelStyle={style.textButton}
-        >
-          <Text>Início</Text>
-        </Button>
-      </View>
-
-      <TextInput
-        placeholderTextColor={paperTheme.colors.text}
-        style={style.searchInput}
-        mode="outlined"
-        placeholder="O que deseja buscar?"
+    <SafeAreaView style={style.main}>
+      <ShopTopMenu
+        title="Serviços"
+        description={unitStore.name || unitStore.store.name}
+        unitStore={unitStore}
       />
 
-      <ScrollView
-        horizontal={true}
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}
-      >
-        <View style={[style.tagsContainer, style.inlineView]}>
-          {/*fazer um mapping ai*/}
-          <Tag selected={true}>Tudo</Tag>
-          <Tag>Promoções</Tag>
-          <Tag>Bebidas</Tag>
-          <Tag>Carnes</Tag>
-          <Tag>Cafés, chás e achocolatados</Tag>
-        </View>
-      </ScrollView>
-
-      <ScrollView
-        showsVerticalScrollIndicator={false}
-        showsHorizontalScrollIndicator={false}>
-        <View style={[style.listProductView, style.inlineView]}>
-          {productCategoryList
-            .map((pc, i) => <ProductItem key={"pc-"+i} product={pc.offer} />)}
-        </View>        
-      </ScrollView>
-    </View>
+      <OfferList
+        offers={services}
+        ListHeaderComponent={Header(props)}
+        ListFooterComponent={Footer(props)}
+        seeMoreItemOnPress={() =>
+          navigation.navigate('ShopOffers', route.params)
+        }
+      />
+    </SafeAreaView>
   );
+}
+
+function Header(props: ShopOffersProps) {
+  const {route, navigation} = props;
+  const {unitStore, userAccountStore, services} = route.params;
+
+  return <View></View>;
+}
+
+function Footer(props: ShopOffersProps) {
+  return <View></View>;
 }
