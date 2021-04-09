592b7c24eecf81fc4165ec1eaf744fd42852c7d8
Conrado Pinheiro <conradopsa@gmail.com>
2021-04-08 23:05:40 -0300

    Shophome adjustments

--- src/pages/shop/ShopHome.tsx
+++ src/pages/shop/ShopHome.tsx
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import ShopProfile from '../../containers/shop/ShopProfile';
 import IconIonicons from 'react-native-vector-icons/Ionicons';
 import IconAntDesign from 'react-native-vector-icons/AntDesign';
@@ -7,7 +7,7 @@ import CardTransaction from '../../containers/shop/CardTransaction';
 import {Button} from 'react-native-paper';
 import {UnitStore, UserAccountStore} from '../../types/domain/interfaces';
 import {ScrollView} from 'react-native-gesture-handler';
-import {colors} from '../../theme';
+import {colors, fonts} from '../../theme';
 import MaskedView from '@react-native-community/masked-view';
 import {ViewGradient} from '../../components/ViewGradient';
 import IconMaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
@@ -18,14 +18,20 @@ import {StackScreenProps} from '@react-navigation/stack';
 import {ShopStackParamList} from '../../routes/ShopRoutes';
 import {ShopTopMenu} from '../../containers/shop/ShopTopMenu';
 import {OfferList} from '../../containers/shop/OfferList';
+import {Text} from '../../components/Text';
 
 const style = StyleSheet.create({
   main: {
     flex: 1,
     backgroundColor: colors.white,
   },
-  offers: {
-    marginTop: 30
+  serviceView: {
+    marginTop: 30,
+  },
+  serviceText: {
+    marginLeft: 20,
+    fontFamily: fonts.semiBoldFontFamily,
+    fontSize: 15
   }
 });
 
@@ -46,7 +52,10 @@ export default function ShopHome({route, navigation}: ShopHomeProps) {
           userAccountStore={userAccountStore}
         />
 
-        <OfferList style={style.offers} offers={services} />
+        <View style={style.serviceView}>
+          <Text style={style.serviceText}>Serviços</Text>
+          <OfferList offers={services} limit={4} />
+        </View>
       </ScrollView>
     </SafeAreaView>
   );
